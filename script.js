af776b7c06dbbf2d421d01812f4b6d08f8e4723e
Kovalskii Stanislav <32917606+Hikaari@users.noreply.github.com>
2024-10-29 05:54:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showDescription(index) {
+    const description = document.getElementById(`desc${index}`);
+    description.style.opacity = description.style.opacity === '1' ? '0' : '1';
+}
+
